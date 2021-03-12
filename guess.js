9ad0d2c1559725081179169a3a0e25b01c1bd874
drtdenis <drouetdenis2@gmail.com>
2021-03-12 17:30:04 +0100

    exam.js

---
+++ guess.js
@@ -0,0 +1,16 @@
+/*Ecrire une fonction guess qui prend 2 nombres en paramètres userGuess et secret.
+Si userGuess est inférieur à secret la fonction devra afficher: Too small!
+Si userGuess est supérieur à secret la fonction devra afficher: Too big!
+Si userGuess est égal à secret la fonction devra afficher: You win*/
+
+let userGuess = 2
+let secret = 5
+
+if ( userGuess < secret) {
+  console.log('too small!')
+} else if (userGuess > secret) {
+  console.log('Too big!')
+} else if 
+  (userGuess = secret){
+    console.log('You win')
+  }
